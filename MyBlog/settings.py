18b61c76417977565716070b5a529663126c13ac
Patrick Wilson <patrick@patrick-Inspiron-7520.(none)>
2013-08-17 14:50:18 -0700

    blah

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -106,7 +106,7 @@ ROOT_URLCONF = 'MyBlog.urls'
 WSGI_APPLICATION = 'MyBlog.wsgi.application'
 
 TEMPLATE_DIRS = (
-    PROJECT_PATH
+    os.path.join(PROJECT_PATH, '..')
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
